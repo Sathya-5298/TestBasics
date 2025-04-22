4c9e62991c9e6e0ebd5514c9bb4626a249fefbd0
sdevalap <sdevalap@LIN-CR2C2J3.corp.capgemini.com>
2025-04-22 18:24:50 +0530

    mnb

--- src/test/java/com/practise/qa/seleniumtests/LinksCount.java
+++ src/test/java/com/practise/qa/seleniumtests/LinksCount.java
@@ -11,21 +11,40 @@ public class LinksCount {
 
 	public static void main(String[] args) 
 	{
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.actitime.com/");
+		/*
+		 * WebDriver driver = new ChromeDriver(); driver.manage().window().maximize();
+		 * driver.get("https://www.actitime.com/");
+		 * 
+		 * List<WebElement> links = driver.findElements(By.xpath("//a"));
+		 * 
+		 * System.out.println(links.size());
+		 * 
+		 * for(WebElement s : links) { System.out.println(s.getText());
+		 * System.out.println(s.getDomAttribute("class"));
+		 * System.out.println("============="); } driver.close();
+		 */
 		
-		List<WebElement> links = driver.findElements(By.xpath("//a"));
+		System.out.println(5<<2);
 		
-		System.out.println(links.size());
+		int x = 5;
+		int y = 10;
 		
-		for(WebElement s : links)
-		{
-			System.out.println(s.getText());
-			System.out.println(s.getDomAttribute("class"));
-			System.out.println("=============");
+		first:{
+			second:
+			{
+				third:
+				{
+					if(x==y >> 1) 
+					{
+						break second;
+					}
+				}
+				System.out.println(x);
+			}
+			System.out.println(y);
 		}
-		driver.close();
+		
+		
 	}
 
 }
